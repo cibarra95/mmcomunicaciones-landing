e0a00e83f23ae53fef6b5eb469a941aae26bae7e
Cesar Ibarra <cesarij07@gmail.com>
2024-08-18 15:38:27 -0600

    chore(config): add all config

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
+<script lang="ts" setup>
+import { useHead, useSeoMeta } from 'unhead';
+
+useHead({
+  meta: [
+    {
+      name: 'viewport',
+      content:
+        'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+    },
+  ],
+});
+
+useSeoMeta({
+  titleTemplate: 'mm-comunicaciones',
+  title: 'mm-comunicaciones',
+  ogTitle: 'mm-comunicaciones',
+  description: '',
+  lang: 'es',
+  ogDescription: 'mm-comunicaciones',
+  ogType: 'website',
+  ogUrl: 'https://mm-comunicaciones.com.mx',
+  ogSiteName: 'mm-comunicaciones',
+  ogImageAlt: 'mm-comunicaciones',
+  ogLocale: 'es_MX',
+});
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="min-h-screen text-gray-800">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ components/navigation/Navbar.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+const navigation = inject<Ref<NavItem[]>>('navigation', ref([]));
+
+const links = [
+  {
+    label: 'Docs',
+    icon: 'i-heroicons-book-open',
+    to: '/getting-started',
+  },
+  {
+    label: 'Pro',
+    icon: 'i-heroicons-square-3-stack-3d',
+    to: '/pro',
+  },
+  {
+    label: 'Releases',
+    icon: 'i-heroicons-rocket-launch',
+    to: '/releases',
+  },
+];
+</script>
+
+<template>
+  <UHeader :links="links">
+    <template #logo>
+      <Logo class="w-auto h-6" />
+    </template>
+
+    <template #right>
+      <UColorModeButton />
+
+      <UButton
+        color="gray"
+        icon="i-simple-icons-github"
+        target="_blank"
+        to="https://github.com/nuxt/ui"
+        variant="ghost"
+      />
+    </template>
+
+    <template #panel>
+      <UNavigationTree :links="mapContentNavigation(navigation)" />
+    </template>
+  </UHeader>
+</template>
